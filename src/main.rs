6b91c4ee3f8214a2697638fc38c429488a90148f
Hamza <muhammadhamza1311@gmail.com>
2021-05-16 01:12:28 +0500

    Heroku pt2

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,22 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
-    let figment = Figment::from(rocket::Config::default()).merge((
-        "port",
-        std::env::var("PORT")
-            .unwrap_or_else(|_| "8000".to_string())
-            .as_str()
-            .parse::<u16>()
-            .expect("invalid port supplied"),
-    ));
+    let figment = Figment::from(rocket::Config::default())
+        .merge((
+            "port",
+            std::env::var("PORT")
+                .unwrap_or_else(|_| "8000".to_string())
+                .as_str()
+                .parse::<u16>()
+                .expect("invalid port supplied"),
+        ))
+        .merge((
+            "address",
+            std::env::var("HOST")
+                .unwrap_or_else(|_| "0.0.0.0".to_string())
+                .as_str()
+                .parse::<IpAddr>()
+                .expect("invalid host supplied"),
+        ));
     rocket::custom(figment).mount("/", routes![index])
 }
